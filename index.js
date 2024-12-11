0493f7d55d1f0aa9fd3a1c4c1db0e5b2f7d7a47f
mahmoodiftee <mahmoodiftee@gmail.com>
2024-12-12 04:10:36 +0600

    added routes for posting and getting lessons

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://your-production-frontend.com"],
+  })
+);
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fc0zsls.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,6 +36,7 @@ async function run() {
     // Get the database and collection
     const db = client.db("Learn");
     const lessonsCollection = db.collection("lessons");
+    const ytLinksCollection = db.collection("ytLinks");
 
     // Create a route to fetch all lessons
     app.get("/lessons", async (req, res) => {
@@ -40,6 +46,38 @@ async function run() {
       } catch (error) {
         console.error("Error fetching lessons:", error);
         res.status(500).json({ error: "Failed to fetch lessons" });
+      }
+    });
+    // Create a route to fetch all tutorials
+    app.get("/tutorials", async (req, res) => {
+      try {
+        const tutorials = await ytLinksCollection.find({}).toArray();
+        res.status(200).json(tutorials);
+      } catch (error) {
+        console.error("Error fetching lessons:", error);
+        res.status(500).json({ error: "Failed to fetch lessons" });
+      }
+    });
+
+    app.post("/lessons", async (req, res) => {
+      try {
+        const { lesson, title, description } = req.body;
+        const existingLesson = await lessonsCollection.findOne({ lesson });
+
+        if (existingLesson) {
+          return res.status(400).json({
+            error: "Lesson already exists",
+          });
+        }
+        const newLesson = { lesson, title, description };
+        const result = await lessonsCollection.insertOne(newLesson);
+        res.status(201).json({
+          message: "Lesson added successfully",
+          lesson: newLesson,
+        });
+      } catch (error) {
+        console.error("Error adding lesson:", error);
+        res.status(500).json({ error: "Failed to add lesson" });
       }
     });
   } catch (error) {
